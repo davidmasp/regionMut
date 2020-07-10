43eb82d1d67a733afe53e772fee84ddc359ad26d
david mas <david.mas.p@gmail.com>
2020-07-10 18:42:25 +0200

    addding script to make bins

--- R/genomeBins.R
+++ R/genomeBins.R
@@ -1,129 +1,165 @@
 # Bining
 
 
+binSignal_uniqueRes <- function(gr,
+                                    min_instances = 10,
+                                    min_value = 1,
+                                    n_bins = 4) {
+
+  ## here I obtain the available resolutions of the original file.
+  params_resolutions = table(GenomicRanges::width(dat_bw))
+
+  # this tells us how minimun number of regions are we allow to drop if
+  # they are not at the same res as the rest of the fail. If it is bigger
+  # than this number the function will break
+  params_minInstances = min_instances
+
+  if (any(params_resolutions < params_minInstances)){
+    params_res <- names(params_resolutions[params_resolutions > params_minInstances])
+  } else if (length(params_resolutions) == 1){
+    params_res <- names(params_resolutions)
+  } else {
+    stop("Problem happened with multiple resolutions, check the bw file")
+  }
+
+  if (length(params_res) == 1){
+    params_res = as.integer(params_res)
+  } else {
+    stop("Problem happened with multiple resolutions (2), check the bw file")
+  }
+
+  # now we kill the wrong resolution
+  ol = length(dat_bw)
+  dat_bw = dat_bw[GenomicRanges::width(dat_bw) == params_res]
+  el = length(dat_bw)
+  per = scales::percent(el/ol)
+  message(glue::glue("After removing not uniform resolution values, {per} regions kept."))
 
 
+  ### STEP1
+  # Should be 1 if it is signal/input, then it is equivalent to log(1) = 0
+  # so if the score in in log(fc) this value should be 0.
+  params_min_value = min_value
 
+  # these are the bins which don't account for the minimum valkue, thus
+  # they are sent to the bin 0! (aka baseline)
+  dat_bin0 = dat_bw[dat_bw$score <= params_min_value]
 
-# ALG)
-#
-#   0) pre-processing
-#   1) Separate the 0 values
-#   2) sort
-#   3) make a quantiles using indeces
-#
+  dat_bw_filt = dat_bw[dat_bw$score > params_min_value]
+  # dat_bw_filt$score %>% log %>% hist
 
-# data --------------------------------------------------------------------
-library(magrittr)
-library(GenomicRanges)
-fname = "~/../Desktop/wgEncodeBroadHistoneNhekCtcfStdSig.bigWig"
-dat_bw <- rtracklayer::import.bw(fname)
+  ### STEP2
+  # we order the bins according to their score
+  dat_bw_filt = dat_bw_filt[order(dat_bw_filt$score)]
 
+  ### STEP3
+  # then we bin the genome according to the argument number of bins/
+  # because the resolution is shared they will have the same genome size.
+  labels = glue::glue("eqFreqBin{seq_len(n_bins)}of{n_bins}")
+  bin_values = cut(seq_len(length(dat_bw_filt)),
+                   breaks = n_bins,
+                   labels = labels)
 
-# solve resolution prob ---------------------------------------------------
-params_resolutions = table(width(dat_bw))
+  dat_bw_filt$bins = bin_values
 
-params_minInstances = 10
+  ## Now I introduce the info in the 0 bins
+  dat_bin0$bins = glue::glue("eqFreqBin0of{n_bins}")
 
-if (any(params_resolutions < params_minInstances)){
-  params_res <- names(params_resolutions[params_resolutions > params_minInstances])
-} else if (length(params_resolutions) == 1){
-  params_res <- names(params_resolutions)
-} else {
-  stop("Problem happened with multiple resolutions, check the bw file")
+  dat_result = c(dat_bin0,dat_bw_filt)
+  dat_result = GenomicRanges::sort(dat_result)
+  dat_result
 }
 
-if (length(params_res) == 1){
-  params_res = as.integer(params_res)
-} else {
-  stop("Problem happened with multiple resolutions (2), check the bw file")
+
+
+
+binSignal_cumSum <- function(gr,
+                             min_value = 1,
+                             n_bins = 4) {
+
+  ## see comments above
+
+  params_min_value = min_value
+
+  dat_bin0 = dat_bw[dat_bw$score <= params_min_value]
+  dat_bw_filt = dat_bw[dat_bw$score > params_min_value]
+
+  dat_bw_filt = dat_bw_filt[order(dat_bw_filt$score)]
+  w_values = GenomicRanges::width(dat_bw_filt)
+  dat_bw_filt$cumWidth = cumsum(as.numeric(w_values))
+
+  max_genone = dat_bw_filt[length(dat_bw_filt)]$cumWidth
+
+  # this generates equal sized bins from the minimum value to the maximum
+  cut_points = seq(from = params_min_value,
+                   to = max_genone,
+                   ## I think this + 1 is because these are cutpoints
+                   ## so it will generate one bin less.
+                   length.out = n_bins + 1)
+
+  labels = glue::glue("eqFreqBin{seq_len(n_bins)}of{n_bins}")
+  bin_values = cut(dat_bw_filt$cumWidth,
+                   breaks = n_bins,
+                   labels = labels)
+
+  dat_bw_filt$bin_values = bin_values
+
+  dat_bin0$bin_values = glue::glue("eqFreqBin0of{n_bins}")
+
+  dat_result = c(dat_bin0,dat_bw_filt)
+  dat_result = GenomicRanges::sort(dat_result)
+  dat_result
+
 }
 
-# now we kill the wrong resolution
-ol = length(dat_bw)
-dat_bw = dat_bw[width(dat_bw) == params_res]
-el = length(dat_bw)
-per = scales::percent(el/ol)
-message(glue::glue("After removing not uniform resolution values, {per} samples kept."))
 
 
-### STEP1
-# this is equivalent to log(1) = 0
-# I think this should be like signal / input so 1 is the new 0
-params_min_value = 1
+parallel_intersect <- function(gr_score,gr_mask){
 
-dat_bin0 = dat_bw[dat_bw$score <= params_min_value]
+  ## this functions takes a gr object with an score column
+  ## (coming from a bw file)
+  ## and removes the parts which are not in the
+  ## gr_mask by parallel intersection
+  ## Keeping original scores to the split ranges.
+  if (!any(seqnames(gr_score) %in% seqnames(gr_mask))){
+    stop("No shared seqnames")
+  }
 
-dat_bw_filt = dat_bw[dat_bw$score > params_min_value]
-dat_bw_filt$score %>% log %>% hist
+  mcols(gr_mask) = NULL
 
+  if (!isDisjoint(gr_mask)){
+    stop("Mask should not have overlaping segments")
+  }
 
-### STEP2
-dat_bw_filt = dat_bw_filt[order(dat_bw_filt$score)]
+  if (!isDisjoint(gr_score)){
+    stop("GR score should not have overlaping segments")
+  }
 
-### STEP3
-n_bins = 4
-labels = glue::glue("eqFreqBin{seq_len(n_bins)}of{n_bins}")
-bin_values = cut(seq_len(length(dat_bw_filt)),breaks = n_bins,labels = labels)
-dat_bw_filt$bins = bin_values
+  ## both mask and score gr objects should be reduced here.
+  ## Mask we can do but score?
 
-dat_bw_filt$width = width(dat_bw_filt)
+  ovr = findOverlaps(query = gr_score,
+                     subject = gr_mask)
 
-library(ggplot2)
-dat_bw_filt %>% as.data.frame() %>%
-  ggplot(aes(x = score,
-             fill = bin_values)) +
-  geom_histogram(bins = 100) +
-  scale_x_log10()
+  ol = sum(GenomicRanges::width(gr_score))
+  # I am pretty sure duplicated overlaps are not a problem, how to test.
+  gr_score_int = GenomicRanges::pintersect(x = gr_score[queryHits(ovr)],
+                                           y = gr_mask[subjectHits(ovr)])
+  el = sum(GenomicRanges::width(gr_score_int))
 
-dat_bw_filt_or %>% as.data.frame() %>%
-  ggplot(aes(x = score,
-             fill = bins)) +
-  geom_histogram(bins = 100) +
-  scale_x_log10()
+  if(options()[["verbose"]]){
+    message(glue::glue("After the pintersect, {scales::percetn(el/ol)} size remaining"))
+  }
 
+  # I thought I had to assign the score of the whole initial bin
+  # to the resulting intersected bin but it does so automatically
+  # maybe could drop this in function test and move it in the
+  # test sections
+  score_original = gr_score$score
+  resulting_score = score_original[queryHits(ovr)]
+  stopifnot(all(resulting_score == gr_score_int$score))
 
-# first step here is the intersect
+  return(gr_score_int)
 
-params_min_value = 1
-dat_bin0 = dat_bw[dat_bw$score <= params_min_value]
-dat_bw_filt = dat_bw[dat_bw$score > params_min_value]
+}
 
-dat_bw_filt = dat_bw_filt[order(dat_bw_filt$score)]
-dat_bw_filt$cumWidth = cumsum(width(dat_bw_filt))
-
-max_genone = dat_bw_filt[length(dat_bw_filt)]$cumWidth
-
-n_bins = 4
-cut_points = seq(from = params_min_value,
-                 to = max_genone,
-                 length.out = n_bins + 1)
-labels = glue::glue("eqFreqBin{seq_len(n_bins)}of{n_bins}")
-bin_values = cut(dat_bw_filt$cumWidth,breaks = n_bins,labels = labels)
-table(bin_values)
-dat_bw_filt$bin_values = bin_values
-
-
-dat_bw_filt_or = dat_bw_filt
-
-# From bed to bw
-#
-# I didn't find a way to do this in bedtools but I may have missed it.
-#
-#   1) make resolution bins of the genome
-#   2) intersect with cds / genes / exons
-#   3) report percentatges
-#
-
-params_res = 1000
-genome = helperMut::genome_selector()
-chunks = helperMut::get_region_chunks(gr = genome,wl = params_res)
-
-library(TxDb.Hsapiens.UCSC.hg19.knownGene)
-txdb = TxDb.Hsapiens.UCSC.hg19.knownGene
-genes(txdb) -> genes_gr
-
-
-
-
-### make p intersect function
