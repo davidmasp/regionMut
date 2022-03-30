2056159ea3073da6ec6199102ee49b098dfbe130
David Mas-Ponte <david.mas.p@gmail.com>
2022-03-29 18:16:11 -0700

    changing wrapper to a python package

--- regionmut/helpers.py
+++ regionmut/helpers.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-
 import subprocess
 import re
 import os
 import tempfile
 import sys
+
 
 def find_r_installed():
     """
@@ -58,31 +57,3 @@ def get_exec_path_mode(mode, pkg):
     script = "exec/{}_{}.R".format(pkg, mode)
     return get_exec_path(script, pkg)
 
-
-## i am going for the simpler version first
-## there might be a way to do this with click
-## but i am not sure how to do it
-
-## EXEC example
-## python3 regionmut.py bin -m exec -p pkg -o out
-
-# sys.argv is the list of command-line arguments.
-
-def main():
-    if len(sys.argv) == 0:
-        raise ValueError("No arguments passed")
-    # 0 is script
-    mode_sel = sys.argv[1]
-    other_args = sys.argv[2:]
-    args_str = " ".join(other_args)
-    print("mode -> {}".format(mode_sel))
-    print("args -> {}".format(args_str))
-    path_to_file = get_exec_path_mode(mode_sel, "regionMut")
-    print("path to file -> {}".format(path_to_file))
-    print(other_args)
-    other_args.insert(0, path_to_file)
-    print("full_cmd -> {}".format(" ".join(other_args)))
-    subprocess.run(other_args, check=True)
-
-if __name__ == "__main__":
-    main()
